315044a647afaeb0a8e392afe420ee339ca0e850
Kevin Garvey <kmgarvey279@gmail.com>
2020-04-11 16:01:53 -0700

    Updates

---
+++ precache-manifest.833e3b9231c0a96d1b0b07adddef28aa.js
@@ -0,0 +1,562 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1ad35b473a9d076001cb8b8e5db181d",
+    "url": "/cryostat-game/index.html"
+  },
+  {
+    "revision": "3e27af398b32ca2eb47a",
+    "url": "/cryostat-game/static/css/main.b81a473b.chunk.css"
+  },
+  {
+    "revision": "bce7719b5822955b5e47",
+    "url": "/cryostat-game/static/js/2.2ec7f892.chunk.js"
+  },
+  {
+    "revision": "3e27af398b32ca2eb47a",
+    "url": "/cryostat-game/static/js/main.8ac0d002.chunk.js"
+  },
+  {
+    "revision": "17c9514806da12048b91",
+    "url": "/cryostat-game/static/js/runtime-main.c48f8836.js"
+  },
+  {
+    "revision": "3e0e99565af25bc213bdaffd31596193",
+    "url": "/cryostat-game/static/media/ChunkyExplosion.3e0e9956.mp3"
+  },
+  {
+    "revision": "9fdea6ef4753fe08038cbe89d1e615e1",
+    "url": "/cryostat-game/static/media/Cold Silence.9fdea6ef.mp3"
+  },
+  {
+    "revision": "a4b3024f9771e05337d97a8ba6cd976a",
+    "url": "/cryostat-game/static/media/Error or failed.a4b3024f.mp3"
+  },
+  {
+    "revision": "ecfd9f12caf40b32dc8c7f6f8b68eb6b",
+    "url": "/cryostat-game/static/media/For the king.ecfd9f12.ogg"
+  },
+  {
+    "revision": "13db7ee23977d9e8844c79e30971ac38",
+    "url": "/cryostat-game/static/media/Hum.13db7ee2.wav"
+  },
+  {
+    "revision": "cdb91146752df6564d129e3d409bfea0",
+    "url": "/cryostat-game/static/media/Jingle_Win_01.cdb91146.mp3"
+  },
+  {
+    "revision": "a4f488ac461dcb819954e556ab2a2345",
+    "url": "/cryostat-game/static/media/Jump4.a4f488ac.wav"
+  },
+  {
+    "revision": "b00fff5ece7bcaad54140fe601c9cedb",
+    "url": "/cryostat-game/static/media/Laser_04.b00fff5e.mp3"
+  },
+  {
+    "revision": "ee10d1c6b971e6cc877fa03eca093f4e",
+    "url": "/cryostat-game/static/media/Laser_08.ee10d1c6.mp3"
+  },
+  {
+    "revision": "d59282116b7f500cf41af3da6a4af844",
+    "url": "/cryostat-game/static/media/LunarHarvest.d5928211.mp3"
+  },
+  {
+    "revision": "c302b10bf7c043bd8964e6657bb63f77",
+    "url": "/cryostat-game/static/media/Menu_Select_00.c302b10b.mp3"
+  },
+  {
+    "revision": "83782b7520e3e0ac811ed53102c2d913",
+    "url": "/cryostat-game/static/media/OrbitalColossus.83782b75.mp3"
+  },
+  {
+    "revision": "88a60de25a98137950229001de6f893a",
+    "url": "/cryostat-game/static/media/Piano Loop.88a60de2.wav"
+  },
+  {
+    "revision": "38a2c86a4726e56a1a89b47b0fafb1f2",
+    "url": "/cryostat-game/static/media/Randomize11.38a2c86a.wav"
+  },
+  {
+    "revision": "4b3f92591c27644d497debf7af1f43c8",
+    "url": "/cryostat-game/static/media/Space Sprinkles.4b3f9259.mp3"
+  },
+  {
+    "revision": "ab1c5a3116db897cf2fe05a557596f89",
+    "url": "/cryostat-game/static/media/Spacearray.ab1c5a31.ogg"
+  },
+  {
+    "revision": "e26681b3211e211a09f24220870a8db0",
+    "url": "/cryostat-game/static/media/The_Endless_Journey.e26681b3.mp3"
+  },
+  {
+    "revision": "bc55be9f1e3583811820aa12211bbc02",
+    "url": "/cryostat-game/static/media/WarpDrive_02.bc55be9f.mp3"
+  },
+  {
+    "revision": "7cd73cee88ed1dc41a64a7ae86ec73f7",
+    "url": "/cryostat-game/static/media/Wind.7cd73cee.ogg"
+  },
+  {
+    "revision": "a1584ee375a44b42e28dfd6bc14c5ddd",
+    "url": "/cryostat-game/static/media/anchor_action.a1584ee3.wav"
+  },
+  {
+    "revision": "ae82217aba29fe261295b4651451cc48",
+    "url": "/cryostat-game/static/media/anchor_action.ae82217a.wav"
+  },
+  {
+    "revision": "4d296b9e6cad233309ee30f1d286e80a",
+    "url": "/cryostat-game/static/media/belt-east.4d296b9e.gif"
+  },
+  {
+    "revision": "5aae18a91fb047ea9867397122c818ee",
+    "url": "/cryostat-game/static/media/belt-north.5aae18a9.gif"
+  },
+  {
+    "revision": "8a90acd106d70bb32b90f89f0ba3e04a",
+    "url": "/cryostat-game/static/media/belt-south.8a90acd1.gif"
+  },
+  {
+    "revision": "4d1140da69765447b3fbe5adc24a6594",
+    "url": "/cryostat-game/static/media/belt-west.4d1140da.gif"
+  },
+  {
+    "revision": "c51fe3678e200297b8ad1811cf353a13",
+    "url": "/cryostat-game/static/media/blaine-stand-south.c51fe367.gif"
+  },
+  {
+    "revision": "d88c5348e11b991a3e34c8e74943822b",
+    "url": "/cryostat-game/static/media/blaine-warp-south.d88c5348.gif"
+  },
+  {
+    "revision": "1c7ad2703987020ac0292fbe6ea5ab2a",
+    "url": "/cryostat-game/static/media/blob-back.1c7ad270.gif"
+  },
+  {
+    "revision": "44d0f1fdcffa8a05d14902fae6e32dd8",
+    "url": "/cryostat-game/static/media/blob-front-knockback.44d0f1fd.gif"
+  },
+  {
+    "revision": "b3b04f91231c573df2bbe51b11eee21b",
+    "url": "/cryostat-game/static/media/blob-shock.b3b04f91.gif"
+  },
+  {
+    "revision": "832ab0c19799d52d2c070638c9353b45",
+    "url": "/cryostat-game/static/media/blob.832ab0c1.gif"
+  },
+  {
+    "revision": "56ef0bad328391da03997ff163469718",
+    "url": "/cryostat-game/static/media/block-warp.56ef0bad.gif"
+  },
+  {
+    "revision": "400f83121c28b824fe69cd970326effe",
+    "url": "/cryostat-game/static/media/blockSink.400f8312.gif"
+  },
+  {
+    "revision": "41318be11e330d1386e2f8a9b56acd9c",
+    "url": "/cryostat-game/static/media/boss-beam.41318be1.gif"
+  },
+  {
+    "revision": "82f9aa2ced0366a8d252e28e10763ba9",
+    "url": "/cryostat-game/static/media/boss1-dark.82f9aa2c.gif"
+  },
+  {
+    "revision": "9862177299ef39fea169b929c129e555",
+    "url": "/cryostat-game/static/media/boss1.98621772.gif"
+  },
+  {
+    "revision": "73287b10147194e0283d5d71a0136fe5",
+    "url": "/cryostat-game/static/media/bossSink.73287b10.gif"
+  },
+  {
+    "revision": "99409f7a0fbf22c3f912d09e26ba7f11",
+    "url": "/cryostat-game/static/media/charging.99409f7a.ogg"
+  },
+  {
+    "revision": "e5a5fbeaafec562c005d0099f16c036f",
+    "url": "/cryostat-game/static/media/core.e5a5fbea.gif"
+  },
+  {
+    "revision": "36b9a777df8897eea6d0019805a7fa18",
+    "url": "/cryostat-game/static/media/cryoTank1.36b9a777.gif"
+  },
+  {
+    "revision": "909c3a05a5ca41389b407b37158b3672",
+    "url": "/cryostat-game/static/media/cryoTank2.909c3a05.gif"
+  },
+  {
+    "revision": "eb447a92a1b1ca3e932186152c29ef2b",
+    "url": "/cryostat-game/static/media/cryoTankE1.eb447a92.gif"
+  },
+  {
+    "revision": "822a2008a90ea2224bc81a8675fcb57a",
+    "url": "/cryostat-game/static/media/cryoTankE2.822a2008.gif"
+  },
+  {
+    "revision": "64a76ffa4c18ae10d46543e8bb6c2152",
+    "url": "/cryostat-game/static/media/cryostatNS.64a76ffa.gif"
+  },
+  {
+    "revision": "8f68dacea6ef3aaa63232c302ee3be35",
+    "url": "/cryostat-game/static/media/danger.8f68dace.gif"
+  },
+  {
+    "revision": "2cfccf385386990e148eb45aceda36fc",
+    "url": "/cryostat-game/static/media/door-close-east.2cfccf38.gif"
+  },
+  {
+    "revision": "2fb3509e8b58d4769a939918fb2d782a",
+    "url": "/cryostat-game/static/media/door-close-north-keycard1.2fb3509e.gif"
+  },
+  {
+    "revision": "50f4712916120c92c0f4898840575e4f",
+    "url": "/cryostat-game/static/media/door-close-north.50f47129.gif"
+  },
+  {
+    "revision": "94081b9786790dd4cf1e9eabf4f18fe9",
+    "url": "/cryostat-game/static/media/door-close-west-keycard2.94081b97.gif"
+  },
+  {
+    "revision": "0d13469353544d64de3ca04ec21e88ec",
+    "url": "/cryostat-game/static/media/door-close-west.0d134693.gif"
+  },
+  {
+    "revision": "4f5df345ab6b68776586822f8424415e",
+    "url": "/cryostat-game/static/media/door-open-east.4f5df345.gif"
+  },
+  {
+    "revision": "f055c9de6b1fbcba44d628ee52f1d769",
+    "url": "/cryostat-game/static/media/door-open-north-keycard1.f055c9de.gif"
+  },
+  {
+    "revision": "c903ad86e4f064367c2ba0360ffe8b6f",
+    "url": "/cryostat-game/static/media/door-open-north.c903ad86.gif"
+  },
+  {
+    "revision": "fc9c97bf08a24f9f53319993cc71105b",
+    "url": "/cryostat-game/static/media/door-open-west-keycard2.fc9c97bf.gif"
+  },
+  {
+    "revision": "eeba3f6005d9128ea0b8e36999002fe6",
+    "url": "/cryostat-game/static/media/door-open-west.eeba3f60.gif"
+  },
+  {
+    "revision": "6b4609f37cda0ae4d458a5e027985e8a",
+    "url": "/cryostat-game/static/media/doorClose.6b4609f3.wav"
+  },
+  {
+    "revision": "7799f0c318cfb49bbe5ea4400c89644d",
+    "url": "/cryostat-game/static/media/doorOpen.7799f0c3.wav"
+  },
+  {
+    "revision": "0cedef83fca4a2baa8d47fba6eb69788",
+    "url": "/cryostat-game/static/media/ecofuture.0cedef83.wav"
+  },
+  {
+    "revision": "35f00bf86ad63928ca8adc30728386cf",
+    "url": "/cryostat-game/static/media/entangle.35f00bf8.gif"
+  },
+  {
+    "revision": "28bf490515ef6b71713bf3ccfba33511",
+    "url": "/cryostat-game/static/media/exit.28bf4905.wav"
+  },
+  {
+    "revision": "3e898f06af0b6cd8a0908d290be5dc03",
+    "url": "/cryostat-game/static/media/exit.3e898f06.wav"
+  },
+  {
+    "revision": "8a587bd1cd081443a2be8aaf81858c03",
+    "url": "/cryostat-game/static/media/exit.8a587bd1.wav"
+  },
+  {
+    "revision": "93e7e28cc5a069b29b8e4845d72df65f",
+    "url": "/cryostat-game/static/media/eyeball-damage.93e7e28c.gif"
+  },
+  {
+    "revision": "019318c009749b688b94e766879622bd",
+    "url": "/cryostat-game/static/media/eyeball.019318c0.gif"
+  },
+  {
+    "revision": "ff30467b69b28f5f673dd4155b81ef58",
+    "url": "/cryostat-game/static/media/fire-filter.ff30467b.png"
+  },
+  {
+    "revision": "90c1d864098cd330e3bfd8cb5efaf952",
+    "url": "/cryostat-game/static/media/fireplace.90c1d864.gif"
+  },
+  {
+    "revision": "e1d13962c84047e980fbd3b377345561",
+    "url": "/cryostat-game/static/media/glitch-computer.e1d13962.mp3"
+  },
+  {
+    "revision": "8662fd8ce3789cf1a2c53d0932de7ef1",
+    "url": "/cryostat-game/static/media/heart.8662fd8c.gif"
+  },
+  {
+    "revision": "fc2b70e2d2647cda8f378286fca55d29",
+    "url": "/cryostat-game/static/media/heartless.fc2b70e2.gif"
+  },
+  {
+    "revision": "c6ef7e4e56a408117b6663204c4ecba4",
+    "url": "/cryostat-game/static/media/illegalityPoint.c6ef7e4e.mp3"
+  },
+  {
+    "revision": "a7e41cdc2bec567f8bffc441266626f7",
+    "url": "/cryostat-game/static/media/lava-cool.a7e41cdc.png"
+  },
+  {
+    "revision": "915418445e64021325d78fa5f13c37c1",
+    "url": "/cryostat-game/static/media/lava.91541844.gif"
+  },
+  {
+    "revision": "b831f7b12e054493822f00d6822c9b0e",
+    "url": "/cryostat-game/static/media/lightningRed.b831f7b1.gif"
+  },
+  {
+    "revision": "9aad516576edc5ebd46c4d91d6f4b4ac",
+    "url": "/cryostat-game/static/media/lightningRight.9aad5165.gif"
+  },
+  {
+    "revision": "703a5d118d16acdd34054fcc64c7b088",
+    "url": "/cryostat-game/static/media/lucy-stand-south.703a5d11.gif"
+  },
+  {
+    "revision": "40779ab5da7025950629260d7b3c8176",
+    "url": "/cryostat-game/static/media/magnet_off.40779ab5.wav"
+  },
+  {
+    "revision": "cbe7d76670b072b1187eab94c98d7937",
+    "url": "/cryostat-game/static/media/magnet_off.cbe7d766.wav"
+  },
+  {
+    "revision": "2e574845d96068bce25fe57d66fbd8a1",
+    "url": "/cryostat-game/static/media/magnet_start.2e574845.wav"
+  },
+  {
+    "revision": "6d5c106b6f953b3463ad490d627dcfb2",
+    "url": "/cryostat-game/static/media/magnet_start.6d5c106b.wav"
+  },
+  {
+    "revision": "ffab3d98db3a5c90ffa93de448abe319",
+    "url": "/cryostat-game/static/media/menu-select.ffab3d98.wav"
+  },
+  {
+    "revision": "8f8a8b98c655870f6b0502069cc630bc",
+    "url": "/cryostat-game/static/media/menuClick.8f8a8b98.wav"
+  },
+  {
+    "revision": "5ee1ff24fc6fffc67197957a570e27b0",
+    "url": "/cryostat-game/static/media/metal.5ee1ff24.mp3"
+  },
+  {
+    "revision": "956b745eee68ab928ec1cf614ff179cb",
+    "url": "/cryostat-game/static/media/phone-pick-up.956b745e.wav"
+  },
+  {
+    "revision": "fedcf3aa6216b435b99495532acb1482",
+    "url": "/cryostat-game/static/media/ping.fedcf3aa.mp3"
+  },
+  {
+    "revision": "700907bcb297a6cedbbb940740427e94",
+    "url": "/cryostat-game/static/media/platformOnEW.700907bc.gif"
+  },
+  {
+    "revision": "a22f29d04cadc6611537ffb23791c64b",
+    "url": "/cryostat-game/static/media/platformOnNS.a22f29d0.gif"
+  },
+  {
+    "revision": "d84e0020a1713020bc6e0ca6160a419a",
+    "url": "/cryostat-game/static/media/playerDashSouth.d84e0020.gif"
+  },
+  {
+    "revision": "2c0af6fd91c788ceb58704feb8d3da61",
+    "url": "/cryostat-game/static/media/playerKnockbackNorth.2c0af6fd.gif"
+  },
+  {
+    "revision": "1fb29bb377dd2f4de15515bcd795c595",
+    "url": "/cryostat-game/static/media/playerKnockbackSouth.1fb29bb3.gif"
+  },
+  {
+    "revision": "b34db217bfc03e4ce7fda4b1640f9b37",
+    "url": "/cryostat-game/static/media/playerPunchSouth.b34db217.gif"
+  },
+  {
+    "revision": "2cb3be1f798c7682f44558f106f5b098",
+    "url": "/cryostat-game/static/media/playerStandEast.2cb3be1f.gif"
+  },
+  {
+    "revision": "2f4c95b578c4855fafef4154ab0de843",
+    "url": "/cryostat-game/static/media/playerStandNorth.2f4c95b5.gif"
+  },
+  {
+    "revision": "d525cdd5cc86e6fa97265fe73756e32b",
+    "url": "/cryostat-game/static/media/playerStandSouth.d525cdd5.gif"
+  },
+  {
+    "revision": "7479c1d50162f3fb2f7f1d7c383ed2bc",
+    "url": "/cryostat-game/static/media/playerStandWest.7479c1d5.gif"
+  },
+  {
+    "revision": "3ca78cbc1fbbc7d4de67110a21ea1793",
+    "url": "/cryostat-game/static/media/playerWalkEast.3ca78cbc.gif"
+  },
+  {
+    "revision": "a1e73b410d957bb12d3390b4c0c944ef",
+    "url": "/cryostat-game/static/media/playerWalkEast2.a1e73b41.gif"
+  },
+  {
+    "revision": "99c790f688ede7aa19e562dea3c6810b",
+    "url": "/cryostat-game/static/media/playerWalkNorth.99c790f6.gif"
+  },
+  {
+    "revision": "cc9e0c65f31c4d2776671548c5cbbb50",
+    "url": "/cryostat-game/static/media/playerWalkNorth2.cc9e0c65.gif"
+  },
+  {
+    "revision": "24677e9d36bdf0c8c038ced9a3f7d3f0",
+    "url": "/cryostat-game/static/media/playerWalkSouth.24677e9d.gif"
+  },
+  {
+    "revision": "944915730a5222301950853d8e66bd12",
+    "url": "/cryostat-game/static/media/playerWalkSouth2.94491573.gif"
+  },
+  {
+    "revision": "ce1b9621dce2de80904b723a8d7227fe",
+    "url": "/cryostat-game/static/media/playerWalkWest.ce1b9621.gif"
+  },
+  {
+    "revision": "29c119a22b6d6f65e509dd2e37ce3ccc",
+    "url": "/cryostat-game/static/media/playerWalkWest2.29c119a2.gif"
+  },
+  {
+    "revision": "7d7f1927c891ad999579ea0316d4774b",
+    "url": "/cryostat-game/static/media/potion-drink-regen.7d7f1927.wav"
+  },
+  {
+    "revision": "ba901eb93c34be2a225daed014621746",
+    "url": "/cryostat-game/static/media/punch.ba901eb9.gif"
+  },
+  {
+    "revision": "a1af3e6700bb67a5b0cc2479b5ae2a91",
+    "url": "/cryostat-game/static/media/robot-off.a1af3e67.gif"
+  },
+  {
+    "revision": "6cc9ba1b08031b119ae979954a4e1c7d",
+    "url": "/cryostat-game/static/media/robot-on-south.6cc9ba1b.gif"
+  },
+  {
+    "revision": "f87a0d3b208e5e8d5047635e9ab09500",
+    "url": "/cryostat-game/static/media/robot.f87a0d3b.mp3"
+  },
+  {
+    "revision": "caed088cc3e9e6f8f5b026ca33245703",
+    "url": "/cryostat-game/static/media/scrape-1.caed088c.wav"
+  },
+  {
+    "revision": "9a894558243949a02f18fff0a2083611",
+    "url": "/cryostat-game/static/media/scream.9a894558.mp3"
+  },
+  {
+    "revision": "fe6e245621f38850e220af3e3c48e45f",
+    "url": "/cryostat-game/static/media/shot.fe6e2456.mp3"
+  },
+  {
+    "revision": "7261977d4bd857844624dfde6dcdfda4",
+    "url": "/cryostat-game/static/media/shutdown.7261977d.wav"
+  },
+  {
+    "revision": "ed64f4d13a94370c5128a323a8ed623f",
+    "url": "/cryostat-game/static/media/slide-2.ed64f4d1.wav"
+  },
+  {
+    "revision": "4ffcfc5296d7d8aa5779f655e392e874",
+    "url": "/cryostat-game/static/media/spooky-filter.4ffcfc52.png"
+  },
+  {
+    "revision": "098a2c64f09a17500eb9c32f34a210cf",
+    "url": "/cryostat-game/static/media/switchOff.098a2c64.wav"
+  },
+  {
+    "revision": "06630726e56924e1e1850b77257e5f13",
+    "url": "/cryostat-game/static/media/switchOn.06630726.wav"
+  },
+  {
+    "revision": "990e48334e7eed8e99a676e16db1e8f7",
+    "url": "/cryostat-game/static/media/switchOn.990e4833.gif"
+  },
+  {
+    "revision": "b5bd055e3018f792e9a60409ff9d64d8",
+    "url": "/cryostat-game/static/media/telephone.b5bd055e.mp3"
+  },
+  {
+    "revision": "f3f2839c1106c35f94c68bd40a3f7b2f",
+    "url": "/cryostat-game/static/media/teleport.f3f2839c.wav"
+  },
+  {
+    "revision": "54e9f3dee54a9ba25a271c523c4d3f9c",
+    "url": "/cryostat-game/static/media/tenta-damage.54e9f3de.gif"
+  },
+  {
+    "revision": "ccb0e0e086225b7547469733ced2af4d",
+    "url": "/cryostat-game/static/media/tenta.ccb0e0e0.gif"
+  },
+  {
+    "revision": "7e6e289c55517fb4e29b476596cec706",
+    "url": "/cryostat-game/static/media/terminal-shock.7e6e289c.gif"
+  },
+  {
+    "revision": "c69b0b50523eedf7164405424ccaea34",
+    "url": "/cryostat-game/static/media/terminal.c69b0b50.gif"
+  },
+  {
+    "revision": "706837866d91341fe2b588898d368d7e",
+    "url": "/cryostat-game/static/media/theMachine.70683786.gif"
+  },
+  {
+    "revision": "47bc8108ceaabea35aaf3b2c55e1191a",
+    "url": "/cryostat-game/static/media/theMachineOn.47bc8108.gif"
+  },
+  {
+    "revision": "48e6200e934a9ae304c5ba721d3571fa",
+    "url": "/cryostat-game/static/media/tick.48e6200e.wav"
+  },
+  {
+    "revision": "dce09ae10f56c222aebbee15c8d10632",
+    "url": "/cryostat-game/static/media/tube-monster1.dce09ae1.gif"
+  },
+  {
+    "revision": "a425ae20d19f11304c3fc7fc24217ce6",
+    "url": "/cryostat-game/static/media/unlink.a425ae20.wav"
+  },
+  {
+    "revision": "b6ce24f421f8b6d6ff70f0a1111b71e6",
+    "url": "/cryostat-game/static/media/ville_seppanen.b6ce24f4.mp3"
+  },
+  {
+    "revision": "1fc108536163d1798d59f1a24b9b22fc",
+    "url": "/cryostat-game/static/media/warp.1fc10853.gif"
+  },
+  {
+    "revision": "4c7971dec5401a5f18953256f77aa257",
+    "url": "/cryostat-game/static/media/warpBlue.4c7971de.gif"
+  },
+  {
+    "revision": "60a38fe6f9857237dc6b30c3b3d41dbf",
+    "url": "/cryostat-game/static/media/warpGreen.60a38fe6.gif"
+  },
+  {
+    "revision": "e0725f6029cfc610e0a014c7f0f46dc1",
+    "url": "/cryostat-game/static/media/warpPurple.e0725f60.gif"
+  },
+  {
+    "revision": "8f504b70bdad7a07753c76ae5a5f129c",
+    "url": "/cryostat-game/static/media/warpRed.8f504b70.gif"
+  },
+  {
+    "revision": "f8248f3515b7c57797f9f5eadb00e902",
+    "url": "/cryostat-game/static/media/warpYellow.f8248f35.gif"
+  },
+  {
+    "revision": "677799f3d749ae5cab5bb11a097595da",
+    "url": "/cryostat-game/static/media/whoosh.677799f3.wav"
+  }
+]);
